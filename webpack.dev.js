3ae2a18a8a17cd4a78a050019e8fccd72a6697d2
stone <stone@stones-MBP.home>
2017-08-29 18:42:35 -0400

    prod and dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const common_config = require("./webpack.config");
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+module.exports = merge(common_config, {
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.DATA_URL_BASE': JSON.stringify('http://localhost:5000')
+        })
+    ]
+});
